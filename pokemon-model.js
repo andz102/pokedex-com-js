7192797144abe4b47912da4d2f5cd5dd7cf9ae87
andz102 <116612280+andz102@users.noreply.github.com>
2022-12-04 15:41:28 -0300

    Add files via upload

---
+++ pokemon-model.js
@@ -0,0 +1,16 @@
+class Pokemon{
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    height;
+    weight;
+    hp;
+    attack;
+    defense;
+    special_attack;
+    special_defense;
+    speed;
+    base_experience;
+}
